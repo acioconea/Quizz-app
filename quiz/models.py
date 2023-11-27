680959e90b4ad2b835093bdb84407e53da7deeb3
Cioconea Adina <cioconea.adina@gmail.com>
2023-11-27 21:55:34 +0200

    update

--- quiz/models.py
+++ quiz/models.py
@@ -29,16 +29,18 @@ class Choice(models.Model):
     text = models.CharField(max_length=255)
     is_correct = models.BooleanField(default=False)
 
-class Response(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True, blank=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    selected_answer = models.ForeignKey(Choice, on_delete=models.CASCADE)
-    score = models.IntegerField(default=0)
-
 
 class UserQuizHistory(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
     score = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True)
+
+
+class Response(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    quiz = models.ForeignKey(UserQuizHistory, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    selected_answer = models.ForeignKey(Choice, on_delete=models.CASCADE)
+    score = models.IntegerField(default=0)
+
